2045b575b906a250abe631622bf8297ded62e396
Emelie Brobergs <emebro1@gmail.com>
2021-08-19 15:04:19 +0200

    DELETE middleware created + tested

--- products.controllers.js
+++ products.controllers.js
@@ -17,7 +17,7 @@ function getProducts(req, res, next) {
 // Respond with product based on id, or nothing if not found in DB
 const getOneProductById = (req, res, next) => {
     const id = req.params.id;
-    const product = products.find(product => product.id == id);
+    const product = products.find(item => item.id == id);
     if (!product) {
         res.status(404).json(`Product with id ${id} was not found.`);
     } else {
@@ -35,12 +35,23 @@ function saveProduct(req, res, next) {
 const updateOneProductById = (req, res, next) => {
     const { id } = req.params;
     const newProduct = req.body;
-    //const product = products.find(product => product.id == id);
-    const product = products.map(product => ( product.id == id ? newProduct : product));
+    const product = products.map(item => ( item.id == id ? newProduct : item));
     if (!product) {
         res.status(404).json(`Product with id ${id} was not found.`);
     } else {
         res.status(200).json(product);
+    }
+}
+
+// Delete product by id in DB
+const deleteOneProductById = (req, res, next) => {
+    const id = req.params.id;
+    const product = products.find(item => item.id == id);
+    if (!product) {
+        res.status(404).json(`Product with id ${id} was not found.`);
+    } else {
+        products.splice(products.findIndex(item => item.id === id),1);
+        res.status(200).json(`Product with id ${id} is deleted.`);
     }
 }
 
@@ -49,5 +60,6 @@ module.exports = {
     getProducts,
     getOneProductById,
     saveProduct,
-    updateOneProductById
+    updateOneProductById,
+    deleteOneProductById
 }
--- products.router.js
+++ products.router.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getProducts, getOneProductById, saveProduct, updateOneProductById } = require('./products.controllers');
+const { getProducts, getOneProductById, saveProduct, updateOneProductById, deleteOneProductById } = require('./products.controllers');
 const { saveProductValidation } = require('./products.validation');
 
 // Create the router object
@@ -10,7 +10,7 @@ productRouter.get('/api/products', getProducts);
 productRouter.get('/api/products/:id', getOneProductById);
 productRouter.post('/api/products', saveProductValidation, saveProduct);
 productRouter.put('/api/products/:id', saveProductValidation, updateOneProductById);
-productRouter.delete('/api/products/:id');
+productRouter.delete('/api/products/:id', deleteOneProductById);
 
 // Export the router object
 module.exports = productRouter;
